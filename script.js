2fddf294512c4d3761dd688f879472734ffae9ca
Raima Aftab <raima.aftab2022@gmail.com>
2023-09-28 09:58:54 +0500

    change
    final assignment#4

--- script.js
+++ script.js
@@ -13,10 +13,7 @@ function addTodo() {
 
 taskItem.innerHTML = `<div class="input-container w-100 my-2 d-flex">
         <input type="text" class="form-control" value="${taskText}" readonly>
-        <button class="btn edit-btn p-2 btn-sm edit-todo float-end">
-        <i class="fas fa-edit"></i>
-    </button>
-    <button class="btn trash-btn btn-sm p-2 delete-todo float-end"  onclick="removeTodo(this)">
+    <button class="btn trash-btn  p-2 delete-todo float-end"  onclick="removeTodo(this)">
         <i class="fas fa-trash"></i>
     </button> 
     </div>`;
@@ -42,3 +39,8 @@ console.log(i.parentNode.remove());
 function removeAllTodos() {
 todoList.innerHTML = '';
 }
+
+//edit button will be added later
+    //     <button class="btn edit-btn p-2 btn-sm edit-todo float-end">
+    //     <i class="fas fa-edit"></i>
+    // </button>
